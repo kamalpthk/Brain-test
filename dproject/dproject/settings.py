c8f1cedd93897dfbb996cede189d952cd9d8267f
Kamal <kamalpthk@gmail.com>
2023-09-19 10:40:17 +0530

    Second Initialisation

--- dproject/dproject/settings.py
+++ dproject/dproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xz+rn9#k%mt*g+a_th@z7nx805ymjgugrz)79x)8)4788ic5w!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
