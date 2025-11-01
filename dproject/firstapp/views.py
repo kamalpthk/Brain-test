929903d086bf60ecf89bbc60689d5a0a5ef5b3c1
Kamal <kamalpthk@gmail.com>
2025-11-01 10:45:45 +0530

    added files and edit

--- dproject/firstapp/views.py
+++ dproject/firstapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This a testing website")
+    return HttpResponse("This a awsome testing website")
